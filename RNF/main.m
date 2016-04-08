675407560d668443207999fdbd06bdf49245667d
Michael Borgmann <michaelborgmann@mailbox.org>
2016-04-08 16:35:24 +0200

    Initial Commit

---
+++ RNF/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RNF
+//
+//  Created by Michael Borgmann on 08/04/16.
+//  Copyright © 2016 Michael Borgmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
